09f873bcf7768d56d9fd303d1685cca0655c63b0
dead_line <dead_line@localhost>
2020-05-09 15:09:58 +0900

    Star2

---
+++ YangRepo/src/Star2.java
@@ -0,0 +1,17 @@
+
+public class Star2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		int s;
+		
+		for(i=1; i<=6; i++) {
+			for(s=6; s>i; s--) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
