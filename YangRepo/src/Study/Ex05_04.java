33fd2b88f1f3ebe15ee8647020a11cc460510334
dead_line <dead_line@localhost>
2020-05-11 22:55:44 +0900

    05/11

---
+++ YangRepo/src/Study/Ex05_04.java
@@ -0,0 +1,16 @@
+package Study;
+
+public class Ex05_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		a = 200;
+		
+		if(a<100)
+			System.out.println("100보다 작군요..");
+		else
+			System.out.println("100보다 크군요..");
+	}
+
+}
