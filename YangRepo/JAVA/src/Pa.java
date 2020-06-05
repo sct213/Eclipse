26e45e89be0be19614b36d15a5c9941f0fe2eeb7
DeadLine <yang991211@naver.com>
2020-06-05 11:57:15 +0900

    6/5

---
+++ YangRepo/JAVA/src/Pa.java
@@ -0,0 +1,29 @@
+package src;
+import java.util.*;
+public class Pa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List ar = new ArrayList();
+		
+		ar.add("객체지향 프로그래밍");
+		ar.add("하둡");
+		ar.add("MongoDB");
+		
+		int a = 1;
+		
+		Iterator it = ar.iterator();
+		
+		while(it.hasNext()) {
+			
+			System.out.println(it.next());
+			if(a==1) {
+				System.out.println("<" + it.next() + ">");
+			}else {
+				System.out.println(it);
+			}
+			a++;
+		}
+	}
+
+}
